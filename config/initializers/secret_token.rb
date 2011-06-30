0d5ffe1a79e6f35342e70a5132dd313f08f7a97d
Tony <ptonyweir@gmail.com>
2011-06-30 16:45:40 +1000

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Road04::Application.config.secret_token = '955138e628b25342666671843cb6dec4434ec3c9236c1d5e9c0bf1fa8ff75096ec89a876c6cf2127984814454b1641b4a1b33e80c5d4f9767cffc47e5ac1392f'
